dc88bbec047f7d531b2c2a21e3da4d18b04aace3
DiscordBotsDevelopers20 <39799352+DiscordBotsDevelopers20@users.noreply.github.com>
2018-06-01 18:39:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,5 +31,5 @@ async def on_message(message):
         await client.send_message(message.channel, "**Name:-** {} \n \n **ID:-** {} " .format (message.author , message.author.id))
 
 
-client.run(BOT_TOKEN)
+client.run("NDUyMDk2MTE1OTI4NTk2NDkx.DfLW1Q.7sUDuR-ZRYyeU_GqT5ZyCC03RhI")
 
