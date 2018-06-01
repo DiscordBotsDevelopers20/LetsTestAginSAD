0b318c6561ffd4f43b634e27054d4f16b2396bb3
DiscordBotsDevelopers20 <39799352+DiscordBotsDevelopers20@users.noreply.github.com>
2018-06-01 18:31:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,39 @@
-# These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
 import discord
 import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
-import platform
+import random
+import time
+import datetime
 
-client = Bot(description="Basic Bot by Habchy#1665", command_prefix="-" pm_help = False)
+
+client = discord.Client()
+bot_prefix= ">"
+client = commands.Bot(command_prefix=bot_prefix)
+
+
+
 
 @client.event
 async def on_ready():
-	
-	print('--------')
-	print('Created by Scanner#4797')
-    await client.change_presence(game=discord.Game(name="-HELP | YOU", type=3))
+    print("TO Bot Official")
+    print("Name: {}".format(client.user.name))
+    print("ID: {}".format(client.user.id))
+    await asyncio.sleep (1)
+    await client.change_presence(game=discord.Game(name='Tanki Online | >help', type=0))
+    await asyncio.sleep (3600)
+    await client.change_presence(game=discord.Game(name="Tanki Online Servers", type=3))
+    await asyncio.sleep (3600)
+    await client.change_presence(game=discord.Game(name='Tanki Online | >help', type=0))
 
-@client.command()
-async def ping(*args):
 
-	await client.say(":ping_pong: Pong!")
-	await asyncio.sleep(3)
-	await client.say(":warning: This bot was created by **Habchy#1665**, it seems that you have not modified it yet. Go edit the file and try it out!")
-	
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith(">id"):
+        await client.send_message(message.channel, "**Name:-** {} \n \n **ID:-** {} " .format (message.author , message.author.id))
+
+
 client.run(process.env.BOT_TOKEN)
 
